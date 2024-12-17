35550da50f5324130809a5c097eedb5eaa130860
valentinagn13 <galvisvalentina13@gmail.com>
2024-12-17 10:08:30 -0500

    feat:payment change

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -40,11 +40,22 @@ export class PaymentComponent implements OnInit {
     console.log("Quota ID capturado:", quotaId); // Comprobación en consola
   }
 
+  loading: boolean = false; // Indicador de carga
+
   create() {
+    this.loading = true; // Activa el indicador de carga
     console.log(JSON.stringify(this.payment));
-    this.paymentService.create(this.payment).subscribe((data) => {
-      Swal.fire("PAGO EXITOSO", " se ha pagado exitosa mente", "success"); //tirulo a la alerta
-      this.router.navigate(["quotas/list"]);
+    this.paymentService.create(this.payment).subscribe({
+      next: (data) => {
+        this.loading = false; // Desactiva el indicador de carga
+        Swal.fire("PAGO EXITOSO", "Se realizó el pago exitosamente", "success");
+        this.router.navigate(["quotas/list"]);
+      },
+      error: (err) => {
+        this.loading = false; // Desactiva el indicador en caso de error
+        Swal.fire("ERROR", "Hubo un problema al procesar el pago", "error");
+        console.error(err);
+      },
     });
   }
 }
